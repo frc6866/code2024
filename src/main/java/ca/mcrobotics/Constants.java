4d8bdbc4118ea99f722e5f344a950859c3d11926
Cw168 <nickc.robotics@gmail.com>
2023-11-10 13:39:25 -0500

    Swerve that doesn't work

---
+++ src/main/java/ca/mcrobotics/Constants.java
@@ -0,0 +1,118 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package ca.mcrobotics;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static class CONTROL {
+    public static final int MAIN_XBOX = 0;
+    public static final int OVER_XBOX = 1;
+  }
+
+  public static class DRIVE {
+    //PID stuff
+    public static final double kWheelDiameterMeters = Units.inchesToMeters(4);
+    public static final double kDriveMotorGearRatio = 1 / 5.8462;
+    public static final double kTurningMotorGearRatio = 1 / 18.0;
+    public static final double kDriveEncoderRot2Meter = kDriveMotorGearRatio * Math.PI * kWheelDiameterMeters;
+    public static final double kTurningEncoderRot2Rad = kTurningMotorGearRatio * 2 * Math.PI;
+    public static final double kDriveEncoderRPM2MeterPerSec = kDriveEncoderRot2Meter / 60;
+    public static final double kTurningEncoderRPM2RadPerSec = kTurningEncoderRot2Rad / 60;
+    public static final double kPTurning = 0.5;
+    
+    // CAN IDs for drivetrain motors
+    public static final int FRONT_LEFT_SPEED = 0;
+    public static final int FRONT_RIGHT_SPEED = 1;
+    public static final int BACK_LEFT_SPEED = 2;
+    public static final int BACK_RIGHT_SPEED = 3;
+
+    public static final int FRONT_LEFT_ROTATION = 4;
+    public static final int FRONT_RIGHT_ROTATION = 5;
+    public static final int BACK_LEFT_ROTATION = 6;
+    public static final int BACK_RIGHT_ROTATION = 7;    
+
+    //Encoder for speed/direction motor
+    public static final int FRONT_LEFT_SPEED_ENODER_A = 0;
+    public static final int FRONT_LEFT_SPEED_ENODER_B = 1;
+    public static final int FRONT_RIGHT_SPEED_ENODER_A = 2;
+    public static final int FRONT_RIGHT_SPEED_ENODER_B = 3;
+
+    public static final int BACK_LEFT_SPEED_ENODER_A = 4;
+    public static final int BACK_LEFT_SPEED_ENODER_B = 5;
+    public static final int BACK_RIGHT_SPEED_ENODER_A = 6;
+    public static final int BACK_RIGHT_SPEED_ENODER_B = 7;
+
+    //Encoder for rotation motor
+    public static final int FRONT_LEFT_ROTATION_ENODER_A = 8;
+    public static final int FRONT_LEFT_ROTATION_ENODER_B = 9;
+    public static final int FRONT_RIGHT_ROTATION_ENODER_A = 10;
+    public static final int FRONT_RIGHT_ROTATION_ENODER_B = 11;
+
+    public static final int BACK_LEFT_ROTATION_ENODER_A = 12;
+    public static final int BACK_LEFT_ROTATION_ENODER_B = 13;
+    public static final int BACK_RIGHT_ROTATION_ENODER_A = 14;
+    public static final int BACK_RIGHT_ROTATION_ENODER_B = 15;
+
+    public static final boolean FRONT_LEFT_SPEED_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean FRONT_RIGHT_SPEED_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BACK_LEFT_SPEED_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BACK_RIGHT_SPEED_ABSOLUTE_ENCODER_REVERSED = false;
+
+    public static final boolean FRONT_LEFT_ROTATION_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean FRONT_RIGHT_ROTATION_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BACK_LEFT_ROTATION_ABSOLUTE_ENCODER_REVERSED = false;
+    public static final boolean BACK_RIGHT_ROTATION_ABSOLUTE_ENCODER_REVERSED = false;
+
+    public static final double FRONT_LEFT_SPEED_ABSOLUTE_ENCODER_OFFSET_RAD = -0.254;
+    public static final double FRONT_RIGHT_SPEED_ABSOLUTE_ENCODER_OFFSET_RAD = -1.252;
+    public static final double BACK_LEFT_SPEED_ABSOLUTE_ENCODER_OFFSET_RAD = -1.816;
+    public static final double BACK_RIGHT_SPEED_ABSOLUTE_ENCODER_OFFSET_RAD = -4.811;
+
+    // speed
+    public static final double SPEED = 0.3;
+    public static final double kPhysicalMaxSpeedMetersPerSecond = 5;
+  }
+
+  public static final class CLAMP {
+    // We use the CTREPCM pnuematic control module
+    public static final PneumaticsModuleType PCM_TYPE = PneumaticsModuleType.CTREPCM;
+    // Compressor module number
+    public static final int COMPRESSOR = 0;
+    // PCM port IDs for piston and compressor operation
+    public static final int SOLENOID_FORW = 6;
+    public static final int SOLENOID_REV = 7;
+  }
+
+  public static class ARM {
+    // CAN IDs for arm motors
+    public static final int MOTOR_LEFT_CAN = 22;
+    public static final int MOTOR_RIGHT_CAN = 21;
+    // encoder ports for arm quadrature encoder
+    // (this is just one encoder that uses 4 ports)
+    public static final int ENCODER_A = 1;
+    public static final int ENCODER_B = 2;
+    public static final int ENCODER_C = 3;
+    public static final int ENCODER_D = 4;
+    // limits
+    public static final double LOW_LIMIT = 0.0;
+    public static final double HIGH_LIMIT = 5500.0;
+    public static final double PID_K = 0.3; 
+  }
+
+}
