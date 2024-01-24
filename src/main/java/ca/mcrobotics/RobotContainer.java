ab322c6a38c188fa8240f51bccae696bdbac0feb
Cw168 <nickc.robotics@gmail.com>
2024-01-24 11:34:48 -0500

    E

--- src/main/java/ca/mcrobotics/RobotContainer.java
+++ src/main/java/ca/mcrobotics/RobotContainer.java
@@ -4,11 +4,16 @@
 
 package ca.mcrobotics;
 
+import com.ctre.phoenix.Util;
+
 import ca.mcrobotics.subsystems.*;
 import ca.mcrobotics.subsystems.driveTrain.*;
+import ca.mcrobotics.subsystems.driveTrain.swerveLib.*;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a
@@ -20,9 +25,15 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final CommandXboxController controllerMain = new CommandXboxController(Constants.CONTROL.MAIN_XBOX);
-  private final CommandXboxController controllerAlt = new CommandXboxController(Constants.CONTROL.ALT_XBOX);
-  
+  private final XboxController controllerMain = new XboxController(Constants.CONTROL.MAIN_XBOX);
+
+  public void configureKeybinds() {
+      new JoystickButton(controllerMain, XboxController.Button.kLeftBumper.value);
+  }
+
+  public void teleopPeriodic() {
+    
+  }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
