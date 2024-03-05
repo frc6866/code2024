327f4d4b668d4dd4200cc0e8b80d678cd6ccc6ec
Sam <98060067+Sam629566@users.noreply.github.com>
2024-03-04 23:08:38 -0500

    Double Motor + Fixed Config

--- src/main/java/ca/mcrobotics/subsystems/Flywheel.java
+++ src/main/java/ca/mcrobotics/subsystems/Flywheel.java
@@ -8,15 +8,17 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Flywheel extends SubsystemBase{
-    TalonSRX flywheelMotor;
+public class Flywheel extends SubsystemBase {
+    TalonSRX flywheelMotor, flywheelMotor2;
     ShuffleboardTab tab;
     GenericEntry mtrOut;
     double lastDataSendTime;
     
     public Flywheel() {
         flywheelMotor = new TalonSRX(Constants.Flywheel.FLYWHEEL1_CAN);
+        flywheelMotor2 = new TalonSRX(Constants.Flywheel.FLYWHEEL2_CAN);
         flywheelMotor.setNeutralMode(null);
+        flywheelMotor2.setNeutralMode(null);
 
         lastDataSendTime = Util.staggerUpdates();
         tab = Shuffleboard.getTab("Flywheel");
@@ -26,22 +28,30 @@ public class Flywheel extends SubsystemBase{
     public void configure() {
         flywheelMotor.configNominalOutputForward(0, 30);
         flywheelMotor.configNominalOutputReverse(0, 30);
-        flywheelMotor.configPeakOutputForward(Constants.Template.peakSpeed, 30);
-        flywheelMotor.configPeakOutputReverse(-Constants.Template.peakSpeed, 30);
+        flywheelMotor.configPeakOutputForward(Constants.Flywheel.PEAK_SPEED, 30);
+        flywheelMotor.configPeakOutputReverse(-Constants.Flywheel.PEAK_SPEED, 30);
         flywheelMotor.setSensorPhase(true);
+
+        flywheelMotor2.configNominalOutputForward(0, 30);
+        flywheelMotor2.configNominalOutputReverse(0, 30);
+        flywheelMotor2.configPeakOutputForward(Constants.Flywheel.PEAK_SPEED, 30);
+        flywheelMotor2.configPeakOutputReverse(-Constants.Flywheel.PEAK_SPEED, 30);
+        flywheelMotor2.setSensorPhase(true);
     }
 
     public void stop() {
         flywheelMotor.set(ControlMode.Disabled, 0);
-        // flywheelMotor.set(ControlMode.PercentOutput, 0); // maybe idk ?
+        flywheelMotor2.set(ControlMode.Disabled, 0);
     }
 
     public void move(double value) {
         flywheelMotor.set(ControlMode.PercentOutput, value);
+        flywheelMotor2.set(ControlMode.PercentOutput, value);
     }
 
     public void setReversed(boolean rev) {
         flywheelMotor.setInverted(rev);
+        flywheelMotor2.setInverted(rev);
     }
 
     @Override
@@ -50,6 +60,7 @@ public class Flywheel extends SubsystemBase{
             lastDataSendTime = Util.getSeconds();
             
             Util.putEntry(mtrOut, flywheelMotor.getMotorOutputPercent());
+            Util.putEntry(mtrOut, flywheelMotor2.getMotorOutputPercent());
         }
     }
 }
