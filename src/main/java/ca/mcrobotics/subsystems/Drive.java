be22cf4dd1cda513f073c8c71fb3f5509f33f305
PotatoNato <nxrayman19@gmail.com>
2024-03-17 10:30:52 -0400

    Final swerve fix... I hope...

--- src/main/java/ca/mcrobotics/subsystems/Drive.java
+++ src/main/java/ca/mcrobotics/subsystems/Drive.java
@@ -60,8 +60,8 @@ public class Drive extends SubsystemBase {
     }
 
     public void drive(double left, double right, double anga, double angb) {
-        wheelAngA = anga*11/150;
-        wheelAngB = angb*11/150;
+        wheelAngA = anga*13/150;
+        wheelAngB = angb*13/150;
         if (Controls.swerveIsOn) { //TUrn wheel
             swerveOn = true;
             if (((-flEncoder.getPosition()-wheelAngA) > -0.01 && (-flEncoder.getPosition()-wheelAngA) < 0.01) ||
@@ -86,25 +86,25 @@ public class Drive extends SubsystemBase {
 
     public void setWheelAng() { //automatically puts wheels back into straight position (0 degrees)
         if (Math.abs((-flEncoder.getPosition()-wheelAngA)) > 0.005) {
-            flTurn.set((-flEncoder.getPosition()-wheelAngA)*0.6);
+            flTurn.set((-flEncoder.getPosition()-wheelAngA)*0.3);
         } else if ((-flEncoder.getPosition()-wheelAngA) > -0.001 && (-flEncoder.getPosition()-wheelAngA) < 0.001) {
             flTurn.set(0);
         }
 
         if (Math.abs((-frEncoder.getPosition()-wheelAngB)) > 0.005) {
-            frTurn.set((-frEncoder.getPosition()-wheelAngB)*0.6);
+            frTurn.set((-frEncoder.getPosition()-wheelAngB)*0.3);
         } else if ((-frEncoder.getPosition()-wheelAngB) > -0.001 && (-frEncoder.getPosition()-wheelAngB) < 0.001) {
             frTurn.set(0);
         }
 
         if (Math.abs((-blEncoder.getPosition()-wheelAngB)) > 0.005) {
-            blTurn.set((-blEncoder.getPosition()-wheelAngB)*0.6);
+            blTurn.set((-blEncoder.getPosition()-wheelAngB)*0.3);
         } else if ((-blEncoder.getPosition()-wheelAngB) > -0.001 && (-blEncoder.getPosition()-wheelAngB) < 0.001) {
             blTurn.set(0);
         }
 
         if (Math.abs((-brEncoder.getPosition()-wheelAngA)) > 0.005) {
-            brTurn.set((-brEncoder.getPosition()-wheelAngA)*0.6);
+            brTurn.set((-brEncoder.getPosition()-wheelAngA)*0.3);
         } else if ((-brEncoder.getPosition()-wheelAngA) > -0.001 && (-brEncoder.getPosition()-wheelAngA) < 0.001) {
             brTurn.set(0);
         }
